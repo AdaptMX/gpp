4b2530d2542e6919361217f242007c0a0023977b
Nico Piderman <nico.piderman@gmail.com>
2023-07-05 23:46:45 +0200

    doh

--- lib/gpp/sections/uspva.ex
+++ lib/gpp/sections/uspva.ex
@@ -1,10 +1,3 @@
 defmodule Gpp.Sections.Uspva do
   use Gpp.Sections.Usp, section_id: 9
-  # defstruct [:value, :core, section_id: 9]
-  #
-  # def parse(input) do
-  #   with {:ok, core} <- __MODULE__.Core.parse(input) do
-  #     {:ok, %__MODULE__{value: input, core: core}}
-  #   end
-  # end
 end
