f8567ad9ca8a2a1325b9ff5d602f19d01cb2f75d
Nico Piderman <nico.piderman@gmail.com>
2023-07-06 13:16:37 +0200

    prepare hex release

--- mix.exs
+++ mix.exs
@@ -1,28 +1,51 @@
 defmodule Gpp.MixProject do
   use Mix.Project
 
+  @name "Gpp"
+  @version "0.1.0"
+  @repo_url "https://github.com/Adaptmx/gpp"
+
   def project do
     [
       app: :gpp,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.14",
+      description: "Decode IAB GPP strings including the region specific sections they contain.",
+      package: package(),
+      docs: docs(),
       start_permanent: Mix.env() == :prod,
+      name: @name,
+      source_url: @repo_url,
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:benchee, "~> 1.0", only: :dev},
       {:eflambe, "~> 0.3.0", only: :dev}
+    ]
+  end
+
+  defp package do
+    %{
+      licenses: ["Apache 2"],
+      maintainers: ["Nico Piderman"],
+      links: %{"GitHub" => @repo_url}
+    }
+  end
+
+  defp docs do
+    [
+      main: "Gpp",
+      source_ref: "v#{@version}",
+      source_url: @repo_url
     ]
   end
 end
