f1caca211cb9b2a8df32e0da57662226bead682f
Louis Cameron Booth <me@louiscb.com>
2024-02-01 15:23:09 +0100

    Remove dbg

--- lib/gpp/sections/tcf/segment.ex
+++ lib/gpp/sections/tcf/segment.ex
@@ -6,8 +6,6 @@ defmodule Gpp.Sections.Tcf.Segment do
   Decodes the fields of a TCF segment. Currently only returns for cmp_id and vendor_consents.
   """
   def decode_fields(segment, first_skip_bits, second_skip_bits) do
-    dbg segment
-
     with {:ok, cmp_id, second_input} <-
            Enum.drop(segment, first_skip_bits) |> BitUtil.parse_12bit_int(),
          vendor_list <- Enum.drop(second_input, second_skip_bits),
